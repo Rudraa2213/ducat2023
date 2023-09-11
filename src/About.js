9e921106f8a794ff0c7a4ea87af6b8dd019c0ffe
Rudraa2213 <rudraa2213@gmail.com>
2023-09-11 13:44:13 +0530

    Hello

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Abc() {
+  return "welcome to react js"
+}
+
+export default Abc
+
+export function Abcd(){
+    return "Abcd Is Called Abcd is name export function"
+}
+export function Xyz(){
+    return <h1>Heading One</h1>;
+}
